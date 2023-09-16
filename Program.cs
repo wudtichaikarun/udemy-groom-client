4eb9a229db9f95e2a2ba732790f5eab20f2b5b73
Wudtichai Karun <wudtichai.kar@ascendcorp.com>
2023-09-16 18:22:02 +0700

    feat Unary RPC

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using gRoom.gRPC.Messages;
+
+// groomserver project PORT
+using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new Groom.GroomClient(channel);
+Console.Write("Enter room name to gegister: ");
+var roomName = Console.ReadLine();
+var resp = client.RegisterToRoom(new RoomRegistrationRequest { RoomName = roomName });
+Console.WriteLine($"Room ID: {resp.RoomId}");
+Console.Read();
